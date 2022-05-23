7054ac71d68510c31b5c8a158ad148bfeff435be
Esther Whang <estherwhang@MacBook-Pro-2.local>
2022-05-23 15:57:07 -0700

    push files

---
+++ css_debugging/myscripts.js
@@ -0,0 +1,71 @@
+function myFunction1() {
+    var x = document.getElementById("hint1");
+    if (x.innerHTML === "Hint 1: ") {
+      x.innerHTML = "Hint 1: X Y Selector";
+    } else {
+      x.innerHTML = "Hint 1: ";
+    }
+  }
+  
+  function myFunction2() {
+    var x = document.getElementById("hint2");
+    if (x.innerHTML === "Hint 2: ") {
+      x.innerHTML = "Hint 2: X + Y Selector";
+    } else {
+      x.innerHTML = "Hint 2: ";
+    }
+  }
+  
+  function myFunction3() {
+    var x = document.getElementById("hint3");
+    if (x.innerHTML === "Hint 3: ") {
+      x.innerHTML = "Hint 3: X > Y Selector";
+    } else {
+      x.innerHTML = "Hint 3: ";
+    }
+  }
+  function myFunction4() {
+    var x = document.getElementById("hint4");
+    if (x.innerHTML === "Hint 4: ") {
+      x.innerHTML = "Hint 4: X ~ Y Selector";
+    } else {
+      x.innerHTML = "Hint 4: ";
+    }
+  }
+  
+  function myFunction1() {
+    var x = document.getElementById("hint1");
+    if (x.innerHTML === "Hint 1: ") {
+      x.innerHTML = "Hint 1: X Y Selector";
+    } else {
+      x.innerHTML = "Hint 1: ";
+    }
+  }
+  
+  function myFunction2() {
+    var x = document.getElementById("hint2");
+    if (x.innerHTML === "Hint 2: ") {
+      x.innerHTML = "Hint 2: X + Y Selector";
+    } else {
+      x.innerHTML = "Hint 2: ";
+    }
+  }
+  
+  function myFunction3() {
+    var x = document.getElementById("hint3");
+    if (x.innerHTML === "Hint 3: ") {
+      x.innerHTML = "Hint 3: X > Y Selector";
+    } else {
+      x.innerHTML = "Hint 3: ";
+    }
+  }
+  function myFunction4() {
+    var x = document.getElementById("hint4");
+    if (x.innerHTML === "Hint 4: ") {
+      x.innerHTML = "Hint 4: X ~ Y Selector";
+    } else {
+      x.innerHTML = "Hint 4: ";
+    }
+  }
+  
+    
